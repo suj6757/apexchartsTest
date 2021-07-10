ea7721654c35ed282ba8835e337b9c6f3af4c3d8
suj6757 <suj6757@naver.com>
2021-07-10 16:49:37 +0900

    Bar chart : api v0.7 data

--- src/component/config.js
+++ src/component/config.js
@@ -65,83 +65,8 @@ export const LineChartOption = {
 
 //라인
 export const BarChartOption = {
-    options: {
-        chart: {
-            height: 350,
-            type: 'bar',
-        },
-        plotOptions: {
-            bar: {
-                borderRadius: 10,
-                dataLabels: {
-                position: 'top',
-                },
-            }
-        },
-        dataLabels: {
-            enabled: true,
-            formatter: function (val) {
-                return val;
-            },
-            offsetY: -20,
-            style: {
-                fontSize: '12px',
-                colors: ["#304758"]
-            }
-        },
-        xaxis: {
-            categories: [],
-            position: 'top',
-            axisBorder: {
-                show: false
-            },
-            axisTicks: {
-                show: false
-            },
-            crosshairs: {
-                fill: {
-                type: 'gradient',
-                gradient: {
-                    colorFrom: '#D8E3F0',
-                    colorTo: '#BED1E6',
-                    stops: [0, 100],
-                    opacityFrom: 0.4,
-                    opacityTo: 0.5,
-                }
-                }
-            },
-            tooltip: {
-                enabled: true,
-            }
-        },
-        yaxis: {
-            axisBorder: {
-                show: false
-            },
-            axisTicks: {
-                show: false,
-            },
-            labels: {
-                show: false,
-                formatter: function (val) {
-                    return val;
-                }
-            }
-        },
-        title: {
-            text: '',
-            floating: true,
-            offsetY: 330,
-            align: 'center',
-            style: {
-                color: '#444'
-            }
-        }
-    },
-    series: [{
-        name: '',
-        data: []
-    }],
+    options: {},
+    series: []
 };
 
 //히트맵
--- src/container/Bar.js
+++ src/container/Bar.js
@@ -1,34 +1,144 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactApexChart from 'react-apexcharts';
 import { BarChartOption } from '../component/config';
 
 function Bar(props) {
     const [data, setData] = React.useState(BarChartOption);
-    const [seriesData, setSeriesData] = React.useState([]);
-    const [categoriesData, setCategoriesData] = React.useState([]);
+    const [charData, setCharData] = useState({});
+
+    // React.useEffect(() => {
+    //     setSeriesData(['1', '2', '3', '4', '2']);
+    //     setCategoriesData(['10대', '20대', '30대', '40대', '50대']);
+    // }, []);
 
     React.useEffect(() => {
-        setSeriesData(['1', '2', '3', '4', '2']);
-        setCategoriesData(['10대', '20대', '30대', '40대', '50대']);
-    }, []);
+        // 추후 axios에서 받아오게 변경 예정
+        let resBarDataData = [
+            {
+                "Date": "2021-05-01",
+                "Value": 0.1
+            }, {
+                "Date": "2021-05-02",
+                "Value": 0.1
+            }, {
+                "Date": "2021-05-03",
+                "Value": 0.3
+            }, {
+                "Date": "2021-05-04",
+                "Value": -0.1
+            }, {
+                "Date": "2021-05-05",
+                "Value": -0.2
+            }
+        ];
 
-    React.useEffect(() => {
+        setCharData(resBarDataData);
+
+        let seriesData = [];
+        let categoriesData = [];
+
+        resBarDataData.map((res) => {
+            seriesData.push(res.Value);
+            categoriesData.push(res.Date.slice(-2));
+        });
+
         setData({
-            ...data,
+            series: [{
+                name: 'Cash Flow',
+                // data: [1.45, 5.42, 5.9, -0.42, -12.6, -18.1, -18.2, -14.16, -11.1, -6.09, 0.34, 3.88, 13.07,
+                //   5.8, 2, 7.37, 8.1, 13.57, 15.75, 17.1, 19.8, -27.03, -54.4, -47.2, -43.3, -18.6, -
+                //   48.6, -41.1, -39.6, -37.6, -29.4
+                // ]
+                data: seriesData
+            }],
             options: {
-                xaxis: {
-                    categories: categoriesData,
-                    title: {
-                        text: ''
+                chart: {
+                    type: 'bar',
+                    height: 350
+                },
+                plotOptions: {
+                    bar: {
+                        colors: {
+                            ranges: [{
+                                from: 0,
+                                to: 100,
+                                color: '#a5a5a5'
+                            }, {
+                                from: -100,
+                                to: 0,
+                                color: '#255594'
+                            }]
+                        },
+                        columnWidth: '50%',
+                        dataLabels: {
+                            position: 'top',
+                        }
                     }
                 },
-            },
-            series: [{
-                name : '인원수',
-                data : seriesData
-            }]
+                dataLabels: {
+                    enabled: true,
+                    formatter: function (val) {
+                        return val;
+                    },
+                    offsetY: -22,
+                    style: {
+                        fontSize: '12px',
+                        colors: ["#878787"]
+                    }
+                },
+                grid: {
+                    yaxis: {
+                        lines: {
+                        show: false
+                        }
+                    }
+                },
+                annotations: {
+                    yaxis: [{
+                        y: 0,
+                        y2: null,
+                        strokeDashArray: 0,
+                        borderColor: '#c2c2c2',
+                        fillColor: '#c2c2c2',
+                        opacity: 0,
+                        offsetX: 0,
+                        offsetY: 0,
+                        width: '100%',
+                        yAxisIndex: 0
+                    }]
+                },
+                yaxis: {
+                    show: false,
+                    //show: true,
+                    max: function (val) {
+                        return val + 0.1;
+                    },
+                    min: function (val) {
+                        return val - 0.1;
+                    },
+                    tickAmount: 2
+                },
+                xaxis: {
+                    type: 'category',
+                    // categories: [
+                    //   '01', '02', '03', '04', '05', '06',
+                    //   '07', '08', '09', '10', '11', '12',
+                    //   '13', '14', '15', '16', '17', '18',
+                    //   '19', '20', '21', '22', '23', '24',
+                    //   '25', '26', '27', '28', '29', '30',
+                    //   '31'
+                    // ],
+                    categories: categoriesData,
+                    axisBorder: {
+                        show: false
+                    },
+                    axisTicks: {
+                        show: false,
+                    }
+                }
+            }
         });
-    }, [seriesData, categoriesData]);
+    }, []);
 
     return (
         <>
